f0c16c875e3d69c55699e3604d0388f8c9a53ad3
amal <wickramarathna.amal@gmail.com>
2021-08-29 16:39:41 +0530

    updated

--- zip/src/main/java/com/cloudrevel/base/abstractController.java
+++ zip/src/main/java/com/cloudrevel/base/abstractController.java
@@ -21,7 +21,9 @@ public abstract class abstractController implements Serializable{
 	
 	public static final String CUSTOMER_CREATE_CUSTOMER_ACCOUNT = CUSTOMER + "/create_customer_account";
 	public static final String CUSTOMER_UPDATE_CUSTOMER_ACCOUNT = CUSTOMER + "/update_customer_account";
+	public static final String CUSTOMER_DELETE_CUSTOMER_ACCOUNT = CUSTOMER + "/delete_customer_account";
+	public static final String CUSTOMER_GET_ALL_CUSTOMER_ACCOUNTS = CUSTOMER + "/get_all_customer_accounts";
+	public static final String CUSTOMER_MARK_AS_INACTIVE_CUSTOMER_ACCOUNT = CUSTOMER + "/mark_as_inactive_customer_account";
+	public static final String CUSTOMER_MARK_AS_ACTIVE_CUSTOMER_ACCOUNT = CUSTOMER + "/mark_as_active_customer_account";
 	
-	
-
 }
--- zip/src/main/java/com/cloudrevel/controller/customerController.java
+++ zip/src/main/java/com/cloudrevel/controller/customerController.java
@@ -4,7 +4,11 @@ import java.io.Serializable;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.annotation.RequestScope;
 
@@ -28,13 +32,33 @@ public class customerController extends abstractController implements Serializab
 	private customerService custService;
 	
 	@PostMapping(CUSTOMER_CREATE_CUSTOMER_ACCOUNT)
-	public ResponseEntity<Object> createCustomerAccount(customer customer) throws CloudRevelException{
+	public ResponseEntity<Object> createCustomerAccount(@RequestBody customer customer) throws CloudRevelException{
 		return custService.createCustomerAcount(customer);
 	}
 	
 	@PostMapping(CUSTOMER_UPDATE_CUSTOMER_ACCOUNT) 
-	public ResponseEntity<Object> updateCustomerAccount(customer cust)throws CloudRevelException{
+	public ResponseEntity<Object> updateCustomerAccount(@RequestBody customer cust)throws CloudRevelException{
 		return custService.updateCustomerAccount(cust);
+	}
+	
+	@PutMapping(CUSTOMER_DELETE_CUSTOMER_ACCOUNT)
+	public ResponseEntity<Object> deleteCustomerAccount(@RequestParam Integer id)throws CloudRevelException{
+		return custService.customerMasrkAsDelete(id);
+	}
+	
+	@GetMapping(CUSTOMER_GET_ALL_CUSTOMER_ACCOUNTS)
+	public ResponseEntity<Object> getAllActiveCustomers()throws CloudRevelException{
+		return custService.getAllStatusActiveCustomers();	
+	}
+	
+	@PutMapping(CUSTOMER_MARK_AS_INACTIVE_CUSTOMER_ACCOUNT)
+	public ResponseEntity<Object> customerMarkAsInactive(@RequestParam Integer id)throws CloudRevelException{
+		return custService.customerMarkAsInactive(id);
+	}
+	
+	@PutMapping(CUSTOMER_MARK_AS_ACTIVE_CUSTOMER_ACCOUNT)
+	public ResponseEntity<Object> customerMarkAsActive(@RequestParam Integer id)throws CloudRevelException{
+		return custService.customerMarkAsActive(id);
 	}
 
 }
